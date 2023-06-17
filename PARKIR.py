bda1cd4cf1f8dabb2bdcb927526c39ac20b24cbc
Spinoice72 <fadhilaagil72@gmail.com>
2023-06-18 00:03:50 +0700

    update 1

--- PARKIR.py
+++ PARKIR.py
@@ -1,4 +1,4 @@
-print ("Selamat datang, Apakah anda ingin")
+print ("Selamat datang, Apakah anda ingin....")
 print ("1. Parkir")
 print ("2. Keluar")
 print ("3. Keluar Proggram ?")
@@ -8,4 +8,19 @@ occupied = [ 23, 42, 1, 50, 4, 2, 6,20 ]
 
 if pilih == 1:
     print(f"Parkiran ini ada 50 slot")
-    print(f"Dan nomor tempat parkir yang terpakai ada di {occupied}")
+    print(f"Dan nomor tempat parkir yang terpakai di ")
+
+    for i in occupied:
+        print (i, end=" ")
+        
+    lokasi = int(input("Dimana kamu akan parkir : "))
+    occupied.append(lokasi)
+    for i in occupied:
+        print (i, end=" ")
+
+elif pilih == 2:
+    jamMasuk = int(input("Masukan Jam awal masuk : "))
+    jamKeluar = int(input("Jam Keluar : "))
+    lamanya = jamMasuk - jamKeluar
+    if lamanya <= 0:
+        print("gratis")
